bbdc046fa830e4534bebe3efee8492e15bff8624
Doron Tsur <qballer@gmail.com>
2021-05-05 23:49:59 +0300

    update

--- config.fish
+++ config.fish
@@ -17,3 +17,8 @@ set -Ux PYENV_ROOT $HOME/.pyenv
 set -gx HOST_IP (ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
 
 source ~/.config/fish/private.fish # stuff which should never go on a repo
+
+if set -q ZELLIJ   
+else 
+  zellij
+end
